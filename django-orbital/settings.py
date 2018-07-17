ba51525c29a39cb0d3e362038f565e1a03120089
BEARYLENOVO\Admin <barrylim1996@gmail.com>
2018-07-17 23:31:20 +0800

    Added social auth login error url

--- django-orbital/settings.py
+++ django-orbital/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k=a5%8ht9%p#^q6k6(0+$dyrz4c-x7o6_yzw!@ujk+f@6kv*&c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['moviesforcouchpotatoes.herokuapp.com','localhost']
 
@@ -70,6 +70,8 @@ TEMPLATES = [
         },
     },
 ]
+
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/'
 
 WSGI_APPLICATION = 'django-orbital.wsgi.application'
 
