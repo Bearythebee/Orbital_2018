bef4ceae886036e286ca7f1191cb791962ab5d2c
BEARYLENOVO\Admin <barrylim1996@gmail.com>
2018-07-21 11:54:39 +0800

    Updated urls.py for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url, include
 from . import views
 
-
 urlpatterns = [
 	url(r'^signup/$',views.signup_view,name='signup'),
 	url(r'^login/$',views.login_view,name='login'),
 	url(r'^logout/$',views.logout_view,name='logout'),
 	url(r'^oauth/', include('social_django.urls', namespace='social')),
 	url(r'^profile/$',views.redirect_home,name='home'),
-	]
+]
