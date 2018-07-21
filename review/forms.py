4b598c3b2bff12f9bbe2242a074f1e3e4a7dc524
BEARYLENOVO\Admin <barrylim1996@gmail.com>
2018-07-21 11:44:27 +0800

    Updated forms.py

--- review/forms.py
+++ review/forms.py
@@ -1,11 +1,13 @@
 from django import forms
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm, Textarea, ChoiceField
 from review.models import ShowReview
+
 
 class ShowReviewForm(ModelForm):
     class Meta:
         model = ShowReview
-        fields = ['username','review']
+        fields = ['rating', 'review']
         widgets = {
+            'rating': forms.RadioSelect,
             'review': Textarea(attrs={'cols': 80, 'rows': 5, 'placeholder': "Enter review here..."})
         }
