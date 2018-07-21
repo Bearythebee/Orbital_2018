e37e33ffecd395984869f5d32c4eb754a82170d6
BEARYLENOVO\Admin <barrylim1996@gmail.com>
2018-07-21 13:08:14 +0800

    Updated urls.py in main

--- django-orbital/urls.py
+++ django-orbital/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include
 
+
 urlpatterns = [
-    url(r'',include('mainpage.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^mainpage/',include('mainpage.urls', namespace="mainpage")),
     url(r'^accounts/',include('accounts.urls')),
     url(r'^catalog/',include('catalog.urls')),
     url(r'^review/', include('review.urls')),
-    url(r'^profiles/',include('profiles.urls')),
-    
+    url(r'^profiles/', include('profiles.urls')),
 ]
