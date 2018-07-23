db3f4d725333fade87b822ef024f32c0e1637f14
BEARYLENOVO\Admin <barrylim1996@gmail.com>
2018-07-23 18:02:47 +0800

    Debug import issues in catalog models

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+from django.contrib.auth.models import User
+import numpy as np
 
 class Show(models.Model):
     name = models.CharField(max_length=200)
