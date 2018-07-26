7bef71e586a2edc45beea4a7bf076ccf0cdec81b
BEARYLENOVO\Admin <barrylim1996@gmail.com>
2018-07-26 12:23:51 +0800

    Import paginator

--- profiles/views.py
+++ profiles/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @login_required
 def reviews(request):
